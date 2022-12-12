12836075c33f96928b5501ae38b390bdb6a24c2e
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2022-12-12 21:59:28 +0300

    commit tree 1ead9e79dea33809b510c5aadf119d7a42bea51b

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+const findMatching = drivers.filter(driver => {
+return driver === 'Bobby'
+});
+console.log(findMatching);
+
+const findMatchingB = drivers.filter(driver => {
+    return driver === 'Sammy'
+    });
+    console.log(findMatchingB);
+
+const findMatchingC = drivers.filter(driver => {
+        return driver === ['Bobby', 'bobby']
+        });
+        console.log(findMatchingC);
